15aebe8c7b72887e614295f69bcb6ef0f3dd250d
Michal <mykhbond1960gmail.com>
2021-03-15 15:08:01 +0100

    update dateFormatter.js

--- src/assets/js/dateFormatter.js
+++ src/assets/js/dateFormatter.js
@@ -1,12 +1,11 @@
-// function dateFormatter(date) {
-//     //2024-03-01
-//     let year = date.getFullYear();
-//     let month = date.getMonth();
-//     let day = date.getDate();
-//     let hours = date.getHours();
-//     let minutes = date.getMinutes();
-//     let result = year + "-" + (month < 10 ? "0" + (month + 1) :
-//         (month + 1)) + "-" + (day < 10 ? "0" + day : day) ;
-//     return result;
-// }
-// console.log("wremia"+result)
+function dateFormatter(date) {
+    //2024-03-01
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    return  year + "-" + (month < 10 ? "0" + (month + 1) :
+        (month + 1)) + "-" + (day < 10 ? "0" + day : day) ;
+
+}
